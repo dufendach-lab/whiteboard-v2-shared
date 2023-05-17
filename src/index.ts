138b51c8db5419cd7ed908cad9a00362fffea6e6
Kevin R. Dufendach, MD, MS <kevin.dufendach@cchmc.org>
2023-05-17 16:32:41 -0400

    try to get resources back to root import

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
-export * as model from './models'
-export * as constant from './constants'
+export * from "./unit-data";
+export * from "./wb-encounter";
+export * from "./wb-list";
+export * from "./wb-location";
+export * from "./wb-patient";
+export * from "./wb-resource";
+export * from "./constants/constants"
--- src/models/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./unit-data";
-export * from "./wb-encounter";
-export * from "./wb-list";
-export * from "./wb-location";
-export * from "./wb-patient";
-export * from "./wb-resource";
