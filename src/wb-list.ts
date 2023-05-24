4f82e8cf160bd90833edeb92983efca2000f5894
Kevin Brown <kbrown@g2o.com>
2023-05-24 10:26:59 -0400

    update to List model Entry array element

--- src/wb-list.ts
+++ src/wb-list.ts
@@ -5,10 +5,10 @@ export interface WbList extends WbResource {
   wbResourceType : "WbList",
   identifier : string, // Business identifier
   title : string, // Descriptive name for the list
-  entry : [{ // C? Entries in the list
+  entry : { // C? Entries in the list
     flag : CodeableConcept, // Status/Workflow information about this item
     date : string, // When item added to list
     item : string // R!  Actual entry
     deleted? : boolean, // C? If this item is actually marked as deleted
-  }],
+  }[],
 }
