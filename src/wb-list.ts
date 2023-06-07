aae7c1bd0acf78261ddc54fe08871967e8498fb8
Kevin R. Dufendach, MD, MS <kevin.dufendach@cchmc.org>
2023-06-07 12:22:17 -0400

    move WbListEntry into its own interface

--- src/wb-list.ts
+++ src/wb-list.ts
@@ -2,16 +2,16 @@ import {WbResource} from "./wb-resource";
 import {CodeableConcept, Extension} from "fhir/r4";
 
 export interface WbListEntry { // C? Entries in the list
-  flag : CodeableConcept, // Status/Workflow information about this item
-    date : string, // When item added to list
-  item : string // R!  Actual entry
-  deleted? : boolean, // C? If this item is actually marked as deleted,
-    extension?: Extension[]
+  flag: CodeableConcept, // Status/Workflow information about this item
+  date: string, // When item added to list
+  item: string // R!  Actual entry
+  deleted?: boolean, // C? If this item is actually marked as deleted,
+  extension?: Extension[]
 }
 
 export interface WbList extends WbResource {
-  wbResourceType : "WbList",
-  identifier : string, // Business identifier
-  title : string, // Descriptive name for the list
-  entry : WbListEntry[],
+  wbResourceType: "WbList",
+  identifier: string, // Business identifier
+  title: string, // Descriptive name for the list
+  entry: WbListEntry[],
 }
