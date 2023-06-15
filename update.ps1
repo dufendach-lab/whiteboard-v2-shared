18c7ff185e7291f153eca9278606f40aa8f9e737
Kevin R. Dufendach, MD, MS <kevin.dufendach@cchmc.org>
2023-06-15 12:27:28 -0400

    Remove references to additional projects in update script

--- update.ps1
+++ update.ps1
@@ -3,14 +3,15 @@ $ErrorActionPreference = "Stop"
 npm version patch
 git push
 
-npx tsc
-cd ../frontend
-yarn upgrade wb-shared-models
-Write-Host "Frontend updated successfully"
-
-cd ../api
-yarn upgrade wb-shared-models
-Write-Host "API updated successfully"
-
-cd ../shared
-Write-Host "Congrats, your script executed successfully"
+#
+#npx tsc
+#cd ../frontend
+#yarn upgrade wb-shared-models
+#Write-Host "Frontend updated successfully"
+#
+#cd ../api
+#yarn upgrade wb-shared-models
+#Write-Host "API updated successfully"
+#
+#cd ../shared
+#Write-Host "Congrats, your script executed successfully"
