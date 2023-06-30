75988ce5965e39543d44987bb37f5cefdbaf3eef
Kevin Brown <kbrown@g2o.com>
2023-06-30 14:45:06 -0400

    fix imports

--- src/wb-patient.ts
+++ src/wb-patient.ts
@@ -1,7 +1,5 @@
 import {Extension, HumanName} from "fhir/r4";
 import {WbResource} from "./wb-resource";
-import {WbFlag} from "./wb-flag";
-import {WbProcedure} from "./wb-procedure";
 
 export interface WbPatient extends WbResource {
   wbResourceType: "WbPatient",
