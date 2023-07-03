ba072a83ed90530154d38d6251a8461f5bdd339a
Kevin R. Dufendach, MD, MS <kevin.dufendach@cchmc.org>
2023-07-03 11:16:59 -0400

    change wb-location to use coding for operationalStatus

--- src/wb-location.ts
+++ src/wb-location.ts
@@ -1,11 +1,12 @@
 import {WbResource} from "./wb-resource";
+import {Coding} from "fhir/r4";
 
 export interface WbLocation extends WbResource {
     wbResourceType: "WbLocation",
     name: string,
     identifier: string,
     status: ('active'|'suspended'|'inactive') | undefined,
-    operationalStatus: string | undefined,
+    operationalStatus: Coding | undefined,
     //C=Closed,H=Housekeeping,O=Occupied,U=Unoccupied,K=Contaminated,I=Isolated
     partOf: string | undefined, // ID of another location this is physically a part of
     type: string[] | undefined,
