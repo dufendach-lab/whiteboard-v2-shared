9dcfba7069128d519c3cc97182446e4db08b2cf3
Kevin R. Dufendach, MD, MS <kevin.dufendach@cchmc.org>
2024-01-05 13:51:15 -0500

    uncomment api updating

--- update.ps1
+++ update.ps1
@@ -3,15 +3,14 @@ $ErrorActionPreference = "Stop"
 npm version patch
 git push
 
-#
 #npx tsc
 cd ../unit-conductor/frontend
 yarn upgrade wb-shared-models
 Write-Host "Frontend updated successfully"
 
-#cd ../api
-#yarn upgrade wb-shared-models
-#Write-Host "API updated successfully"
+cd ../api
+yarn upgrade wb-shared-models
+Write-Host "API updated successfully"
 
 cd ../../unit-conductor-shared
 Write-Host "Congrats, your script executed successfully"
