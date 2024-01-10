dd2aa34372a19cc658adce6df04ee573079a7664
Ian Price <ian.price@g2o.com>
2024-01-10 10:14:15 -0500

    adding careTeam to wb-patient so pipes can reference it

--- src/wb-patient.ts
+++ src/wb-patient.ts
@@ -1,10 +1,12 @@
 import {Extension, HumanName} from "fhir/r4";
 import {WbResource} from "./wb-resource";
+import { WbCareTeam } from "./wb-care-team";
 
 export interface WbPatient extends WbResource {
   wbResourceType: "WbPatient",
   name: HumanName,
   gender?: ('male' | 'female' | 'other' | 'unknown'),
   birthDate?: string,
-  extension?: Extension[]
+  extension?: Extension[],
+  careTeam?: WbCareTeam, //TODO: Should the patient hold a reference to the care team, or the other way around?
 }
