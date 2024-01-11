e564c234a25089fcd69dab4cb5564418c7421578
Ian Price <ian.price@g2o.com>
2024-01-11 12:08:22 -0500

    expanding WbLocation statuses

--- src/wb-location.ts
+++ src/wb-location.ts
@@ -5,7 +5,7 @@ export interface WbLocation extends WbResource {
     wbResourceType: "WbLocation",
     name: string,
     identifier: string,
-    status?: ('active'|'suspended'|'inactive'),
+    status?: ('active'|'suspended'|'inactive'|'reserved'|'arriving'|'admission'|'discharge'|'unknown'),
     operationalStatus?: 'closed' | 'housekeeping' | 'occupied' | 'unoccupied' | 'contaminated' | 'isolated',
     partOf?: string, // ID of another location this is physically a part of
     type?: string[],
