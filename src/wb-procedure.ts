8a79a35615e19678b1b3e67cf334d74946e209e2
Ian Price <ian.price@g2o.com>
2024-01-18 12:02:25 -0500

    add identifier to WbProcedure

--- src/wb-procedure.ts
+++ src/wb-procedure.ts
@@ -5,6 +5,7 @@ import { WbPatient } from "./wb-patient";
 
 export interface WbProcedure extends WbResource {
   wbResourceType: "WbProcedure",
+  identifier: string,
   coding: Coding[],
   flags: WbFlag[],
   status: "unknown" | "in-progress" | "preparation" | "not-done" | "on-hold" | "stopped" | "completed" | "entered-in-error",
