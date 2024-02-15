fd988a56b334633c7c1c7d37bd059fea873cb48c
Kevin R. Dufendach, MD, MS <kevin.dufendach@cchmc.org>
2024-02-15 14:13:08 -0500

    add generic data option to WbListEntry

--- src/wb-list.ts
+++ src/wb-list.ts
@@ -1,16 +1,17 @@
 import {WbResource} from "./wb-resource";
 import {CodeableConcept} from "fhir/r4";
 
-export interface WbListEntry { // C? Entries in the list
+export interface WbListEntry<T> { // C? Entries in the list
   flag?: CodeableConcept[], // Status/Workflow information about this item
   date: Date, // When item added to list
   itemId: string // R!  Actual entry
   deleted?: boolean, // C? If this item is actually marked as deleted,
+  data?: T
 }
 
-export interface WbList extends WbResource {
+export interface WbList<T> extends WbResource {
   wbResourceType: "WbList",
   identifier: string, // Business identifier
   title: string, // Descriptive name for the list
-  entries: WbListEntry[],
+  entries: WbListEntry<T>[],
 }
